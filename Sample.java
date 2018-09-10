5ca486d02c483800537a06c8bbe141731595b834
Manju1929 <43138280+Manju1929@users.noreply.github.com>
2018-09-10 18:31:15 +0530

    First commit

---
+++ Sample.java
@@ -0,0 +1,14 @@
+package Pro;
+
+public class Sample {
+	public void Sample1()
+	{
+		System.out.println("hello");
+	}
+public static void main(String args[])
+{
+	Sample s=new Sample();
+	s.Sample1();
+	System.out.println("Hello world");
+}
+}
