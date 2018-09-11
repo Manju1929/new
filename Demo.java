155bf2b374df6acf9284955444561efff66de69c
Manju1929 <43138280+Manju1929@users.noreply.github.com>
2018-09-11 16:11:36 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+public void start()
+{
+int h=10,b=20,l=30;
+int res;
+res=l+b+h;
+System.out.println("Result="+res);
+}
+public static void main(String args[])
+{
+Demo d=new Demo;
+d.start();
+}
+}
